7ec6afcfe2cf4ab1db654d84dfab47cd2f2c1889
michaelstacks <KriskeCrumbly21091996xrjt@hotmail.com>
2025-10-23 19:32:43 +0800

    deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedGame = await deploy("EncryptedGuessingGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedGuessingGame contract: `, deployedGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encryptedGuessingGame"; // id required to prevent reexecution
+func.tags = ["EncryptedGuessingGame"];
