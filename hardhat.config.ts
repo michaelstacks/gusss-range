fc59480e06e2b9f9a040847ff2fa44386654e746
michaelstacks <KriskeCrumbly21091996xrjt@hotmail.com>
2025-10-23 19:32:52 +0800

    update hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,17 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
 
 import "./tasks/accounts";
-import "./tasks/FHECounter";
+import "./tasks/EncryptedGuessingGame";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const MNEMONIC: string = process.env.MNEMONIC ?? "test test test test test test test test test test test junk";
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? "";
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,7 +25,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
   gasReporter: {
@@ -38,6 +39,7 @@ const config: HardhatUserConfig = {
         mnemonic: MNEMONIC,
       },
       chainId: 31337,
+      saveDeployments: true,
     },
     anvil: {
       accounts: {
@@ -47,6 +49,7 @@ const config: HardhatUserConfig = {
       },
       chainId: 31337,
       url: "http://localhost:8545",
+      saveDeployments: true,
     },
     sepolia: {
       accounts: {
